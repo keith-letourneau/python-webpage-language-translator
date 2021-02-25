3eec513455f79bf188871537f8ee8c7a9e11fc1f
Keith <60371048+keith-letourneau@users.noreply.github.com>
2021-02-25 10:58:17 -0500

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -15,49 +15,9 @@ def russian_trans(str):
 
 russian_trans("Let's drink some wine!")
 
-
-#create a list of translations
-ru_text = []
-
-ru_text.append(russian_trans('Tonight we drink bourbon!'))
-ru_text.append(russian_trans('I passed all my classes and exams.'))
-ru_text.append(russian_trans('My water heater is broken.'))
-ru_text.append(russian_trans('Where is the bathroom?'))
-
-for trans in ru_text:
-    print(trans)
-
-
-#save translations to .txt file
-with open(r'C:\Users\keith\OneDrive\Desktop\Python Scripts\Russian Translations.txt', 'w', 
-          encoding="utf-8") as my_file:
-    my_file.write('Common Phrases in Russian!' + '\n' + '\n')
-    for trans in ru_text:
-        my_file.write(trans + '\n')
-
-
-#lets also get pronounciations
-def russian_pronounce(str):
-    pronounce = translator.translate(str, lang_src='en', lang_tgt='ru', pronounce=True)
-    return pronounce[2] + ' // ' + str
-
-russian_pronounce('It is snowing outside.')
-
-ru_pronounce = []
-
-ru_pronounce.append(russian_pronounce('Lets go snowboarding.'))
-ru_pronounce.append(russian_pronounce('Jazz is my favorite music.'))
-ru_pronounce.append(russian_pronounce('I can play guitar'))
-ru_pronounce.append(russian_pronounce('My brother is very strong.'))
-
-for trans in ru_pronounce:
-    print(trans)
-
-
 # # <center>Translate French News Website to English</center>
 # 
 # This script with translate h1,h2,h3 tags to English, place into list and save as .txt file.
-
 
 #translate French news website headlines to English
 import requests
@@ -82,7 +42,6 @@ df['English'] = df['French'].apply(translator.translate, lang_src='fr',
                                                          lang_tgt='en')
 df.head(5)
 
-
 #turn translations into a list and save to .txt file
 fr_headlines = df['English'].values.tolist()
 
@@ -94,4 +53,39 @@ with open(r'C:\Users\keith\OneDrive\Desktop\Python Scripts\French Headlines.txt'
     my_file.write('French Headlines from Today' + '\n' + '\n')
     for headline in fr_headlines:
         my_file.write(headline + '\n')
+     
+#create a list of translations
+ru_text = []
+
+ru_text.append(russian_trans('Tonight we drink bourbon!'))
+ru_text.append(russian_trans('I passed all my classes and exams.'))
+ru_text.append(russian_trans('My water heater is broken.'))
+ru_text.append(russian_trans('Where is the bathroom?'))
+
+for trans in ru_text:
+    print(trans)
+
+#save translations to .txt file
+with open(r'C:\Users\keith\OneDrive\Desktop\Python Scripts\Russian Translations.txt', 'w', 
+          encoding="utf-8") as my_file:
+    my_file.write('Common Phrases in Russian!' + '\n' + '\n')
+    for trans in ru_text:
+        my_file.write(trans + '\n')
+
+#lets also get pronounciations
+def russian_pronounce(str):
+    pronounce = translator.translate(str, lang_src='en', lang_tgt='ru', pronounce=True)
+    return pronounce[2] + ' // ' + str
+
+russian_pronounce('It is snowing outside.')
+
+ru_pronounce = []
+
+ru_pronounce.append(russian_pronounce('Lets go snowboarding.'))
+ru_pronounce.append(russian_pronounce('Jazz is my favorite music.'))
+ru_pronounce.append(russian_pronounce('I can play guitar'))
+ru_pronounce.append(russian_pronounce('My brother is very strong.'))
+
+for trans in ru_pronounce:
+    print(trans)
 
